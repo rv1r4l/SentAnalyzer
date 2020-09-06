84576c272bec25f8b0241f2e57851eb88ad64829
dch777 <d.chanana777@gmail.com>
2020-09-05 21:05:27 -0500

    Updated to latest version

--- node.js
+++ node.js
@@ -1,14 +1,15 @@
 //tag
 let http = require('http');
 let fs = require('fs');
-
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
     if (request.url === "/")
     {
+      response.writeHead(200, {
+          'Content-Type': 'text/html'
+      });
       fs.readFile('./index.html', null, function (error, data) {response.write(data)});
     }
     else {
@@ -21,11 +22,35 @@ let handleRequest = (request, response) => {
               response.writeHead(200, {'Content-type' : 'text/css'});
               response.write(data);
           } else {
+              response.writeHead(200, {
+                  'Content-Type': 'text/html'
+              });
               response.write(data);
           }
           response.end();
       });
     }
 };
+
+var Quiche = require('quiche');
+
+var bar = new Quiche('bar');
+bar.setWidth(400);
+bar.setHeight(265);
+bar.setTitle('Average Sentiment Rating');
+bar.setBarWidth(0);
+bar.setBarSpacing(6); // 6 pixles between bars/groups
+bar.setLegendBottom(); // Put legend at bottom
+bar.setTransparentBackground(); // Make background transparent
+
+bar.addData([-1.1666666666666667], 'CNN', 'FF0000');
+bar.addData([-0.375], 'FOX', '0000FF');
+bar.addData([-0.8666666666666667], 'CBS', '008000');
+bar.addData([-0.9444444444444444], 'CNBC', '00FF00');
+
+bar.setAutoScaling(); // Auto scale y axis
+
+var imageUrl = bar.getUrl(true); // First param controls http vs. https
+console.log(imageUrl);
 
 http.createServer(handleRequest).listen(8080);
